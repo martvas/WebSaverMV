bd0cca4ba5360e5aeb04ad290c329df101428495
Martin <vasiljevs.martins@gmail.com>
2017-12-11 22:32:38 +0100

    11.12 Сохраняет файл на сервере и показывает их клиенту.

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -5,11 +5,6 @@ import Network.ServerSocketThread;
 public class Server {
 
     /*
-    Сейчас:
-    Клиент в новом потоке, создается директория, таблица, можно закинуть файл.
-
-    Сделать:
-    1. Отдавать информацию из БД клиенту о добавленном файле
 
      */
     public static void main(String[] args) {
